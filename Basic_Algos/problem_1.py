2d056be7419b4620960dbfd9b496adb41be015f0
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-01 23:15:03 -0500

    added analysis.txt to Basic_Algos section and time complexity analysis of problem_1.py

--- Basic_Algos/problem_1.py
+++ Basic_Algos/problem_1.py
@@ -1,9 +1,11 @@
 '''
-Find the square root of the integer without using any Python library. You have to find the floor value of the square root.
+Find the square root of the integer without using any Python library. 
+You have to find the floor value of the square root.
 
 For example if the given number is 16, then the answer would be 4.
 
-If the given number is 27, the answer would be 5 because sqrt(5) = 5.196 whose floor value is 5.
+If the given number is 27, 
+the answer would be 5 because sqrt(5) = 5.196 whose floor value is 5.
 
 The expected time complexity is O(log(n))
 
@@ -25,10 +27,10 @@ def sqrt(number):
     return number ** 0.5 // 1
 
 
-######------ DRIVER CODE ------######
-
-print("Pass" if (3 == sqrt(9)) else "Fail")
-print("Pass" if (0 == sqrt(0)) else "Fail")
-print("Pass" if (4 == sqrt(16)) else "Fail")
-print("Pass" if (1 == sqrt(1)) else "Fail")
-print("Pass" if (5 == sqrt(27)) else "Fail")
+######------ DRIVER CODE + TESTS ------######
+if __name__ == '__main__':
+    print("Pass" if (3 == sqrt(9)) else "Fail")
+    print("Pass" if (0 == sqrt(0)) else "Fail")
+    print("Pass" if (4 == sqrt(16)) else "Fail")
+    print("Pass" if (1 == sqrt(1)) else "Fail")
+    print("Pass" if (5 == sqrt(27)) else "Fail")
