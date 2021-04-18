a65eaad4ecb8f2b7038a76198622ee98dc7fee93
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-18 00:38:13 -0500

    refactored prblem_6.py, now more organized/clean and producing expected solution

--- Show_Me_the_Data_Structures/problem_6.py
+++ Show_Me_the_Data_Structures/problem_6.py
@@ -13,8 +13,6 @@ Once you have completed the problem you will
 (ii) perform your own run time analysis on the code.
 '''
 
-# inspiration in java: https://www.geeksforgeeks.org/union-and-intersection-of-two-linked-lists/
-
 
 ############------------ HELPER CODE ------------############
 class Node:
@@ -75,9 +73,8 @@ def is_in(llist, v):
         head_from_llist = head_from_llist.next
     return False
 
+
 ############------------ FUNCTIONS ------------############
-
-
 def union(llist_1, llist_2):
     '''
      creates a variable `n2` to
@@ -101,7 +98,6 @@ def intersection(llist_1, llist_2):
      if value in both "llists", appends element 
      to `intersection_llist`
     '''
-
     intersection_llist = LinkedList()
 
     head_from_llist_1 = llist_1.head
@@ -112,41 +108,51 @@ def intersection(llist_1, llist_2):
             intersection_llist.append(head_from_llist_1.value)
         head_from_llist_1 = head_from_llist_1.next
 
+    if intersection_llist.size() == 0:
+        print("N\A")
     return intersection_llist
 
 
 ############------------ TESTS ------------############
 # Test case 1
 
+def test_case_1():
+    linked_list_1 = LinkedList()
+    linked_list_2 = LinkedList()
 
-linked_list_1 = LinkedList()
-linked_list_2 = LinkedList()
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+    element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
 
-element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
-element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
+    for i in element_1:
+        linked_list_1.append(i)
 
-for i in element_1:
-    linked_list_1.append(i)
+    for i in element_2:
+        linked_list_2.append(i)
 
-for i in element_2:
-    linked_list_2.append(i)
+    print(union(linked_list_1, linked_list_2))
+    print(intersection(linked_list_1, linked_list_2))
+    print()
 
-print(union(linked_list_1, linked_list_2))
-print(intersection(linked_list_1, linked_list_2))
 
 # Test case 2
+def test_case_2():
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
 
-# linked_list_3 = LinkedList()
-# linked_list_4 = LinkedList()
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+    element_2 = [1, 7, 8, 9, 11, 21, 1]
 
-# element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
-# element_2 = [1, 7, 8, 9, 11, 21, 1]
+    for i in element_1:
+        linked_list_3.append(i)
 
-# for i in element_1:
-#     linked_list_3.append(i)
+    for i in element_2:
+        linked_list_4.append(i)
 
-# for i in element_2:
-#     linked_list_4.append(i)
+    print(union(linked_list_3, linked_list_4))
+    print(intersection(linked_list_3, linked_list_4))
 
-# print(union(linked_list_3, linked_list_4))
-# print(intersection(linked_list_3, linked_list_4))
+
+############------------ DRIVER CODE ------------############
+if __name__ == '__main__':
+    test_case_1()
+    test_case_2()
