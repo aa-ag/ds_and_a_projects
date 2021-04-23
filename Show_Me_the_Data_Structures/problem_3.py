572c76ef79db6001b291a84dd3ded7dc5fb2684a
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-22 21:00:42 -0500

    reverted to Udacity/class version of Huffman encoding/decoding for problem_3.py

--- Show_Me_the_Data_Structures/problem_3.py
+++ Show_Me_the_Data_Structures/problem_3.py
@@ -1,106 +1,36 @@
-# inspiration
-# http://zackmdavis.net/blog/2013/06/huffman/
-
 import sys
+import collections
 
 
-class Subtree:
-    def __init__(self, char, freq, left, right):
-        self.char = char
-        self.freq = freq
-        self.left = left
-        self.right = right
-
-    def __gt__(self, othernode):
-        return self.freq > othernode.freq
-
-    def codebook(self):
-        codes = {}
-
-        def traversal(item, code):
-            if item != None:
-                traversal(item.left, code+'0')
-                if item.char != None:
-                    codes[item.char] = code
-                traversal(item.right, code+'1')
-        traversal(self, '')
-        return codes
+def character_frequency(data):
+    return collections.Counter(data)
 
 
-class MinPriorityQueue:
-    def __init__(self):
-        self.queue = []
-
-    def put(self, item):
-        self.queue.append(item)
-        self.queue.sort(reverse=True)
-
-    def get(self):
-        return self.queue.pop()
+def huffman_encoding(data):
+    print(character_frequency(data))
 
 
-def Huffman(C):
-    Q = MinPriorityQueue()
-    leaves = {Subtree(k, C[k], None, None) for k in C}
-    for leaf in leaves:
-        Q.put(leaf)
-    for i in range(len(C)-1):
-        left = Q.get()
-        right = Q.get()
-        new_node = Subtree(None, left.freq + right.freq, left, right)
-        Q.put(new_node)
-    return Q.get().codebook()
+def huffman_decoding(data, tree):
+    pass
 
 
-def code(plaintext, codebook):
-    return ''.join(codebook[c] for c in plaintext)
+if __name__ == "__main__":
+    codes = {}
 
+    a_great_sentence = "The bird is the word"
 
-def decode(ciphertext, codebook):
-    decodebook = {v: k for k, v in codebook.items()}
-    codeword = ''
-    plaintext = ''
-    for i in range(len(ciphertext)):
-        codeword += ciphertext[i]
-        if codeword in decodebook:
-            plaintext += decodebook[codeword]
-            codeword = ''
-    return plaintext
+    print("The size of the data is: {}\n".format(
+        sys.getsizeof(a_great_sentence)))
+    print("The content of the data is: {}\n".format(a_great_sentence))
 
+    encoded_data, tree = huffman_encoding(a_great_sentence)
 
-eng_freqs = {'A': 8167, 'B': 1492, 'C': 2782, 'D': 4253, 'E': 12702,
-             'F': 2228, 'G': 2015, 'H': 6094, 'I': 6966, 'J': 153, 'K': 772,
-             'L': 4025, 'M': 2406, 'N': 6749, 'O': 7507, 'P': 1929, 'Q': 95,
-             'R': 5987, 'S': 6327, 'T': 9056, 'U': 2758, 'V': 978, 'W': 2360,
-             'X': 150, 'Y': 1974, 'Z': 74, ' ': 13000, '.': 4250, ',': 4250}
+    print("The size of the encoded data is: {}\n".format(
+        sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
 
-eng_codebook = Huffman(eng_freqs)
-plain = "I USED TO WONDER WHAT FRIENDSHIP COULD BE, UNTIL YOU ALL SHARED ITS MAGIC WITH ME."
+    # decoded_data = huffman_decoding(encoded_data, tree)
 
-cipher = code(plain, eng_codebook)
-print(cipher)
-
-eng_codebook = Huffman(eng_freqs)
-decipher = decode(cipher, eng_codebook)
-print(decipher)
-
-# if __name__ == "__main__":
-#     codes = {}
-
-#     a_great_sentence = "The bird is the word"
-
-#     print("The size of the data is: {}\n".format(
-#         sys.getsizeof(a_great_sentence)))
-#     print("The content of the data is: {}\n".format(a_great_sentence))
-
-#     encoded_data, tree = huffman_encoding(a_great_sentence)
-
-#     print("The size of the encoded data is: {}\n".format(
-#         sys.getsizeof(int(encoded_data, base=2))))
-#     print("The content of the encoded data is: {}\n".format(encoded_data))
-
-#     decoded_data = huffman_decoding(encoded_data, tree)
-
-#     print("The size of the decoded data is: {}\n".format(
-#         sys.getsizeof(decoded_data)))
-#     print("The content of the encoded data is: {}\n".format(decoded_data))
+    # print("The size of the decoded data is: {}\n".format(
+    #     sys.getsizeof(decoded_data)))
+    # print("The content of the encoded data is: {}\n".format(decoded_data))
