360333c16640767888a51482994696cf17170d2c
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-24 16:03:27 -0500

    both encoding and decodint working properly for problem_3.py: gutted entire algo and started over -- will run class tests next

--- Show_Me_the_Data_Structures/problem_3.py
+++ Show_Me_the_Data_Structures/problem_3.py
@@ -1,80 +1,92 @@
-############------------ IMPORTS ------------############
-import sys
 import collections
-import heapq
 
 
-############------------ FUNCTIONS ------------############
-def count_character_frequency(input_string):
-    return collections.Counter(input_string.replace(' ', ''))
+def frequency(s):
+    freqs = collections.Counter(s)
+    return freqs
 
 
-def huffman_encoding(input_string):
-
-    character_frequency = count_character_frequency(input_string)
-
-    heap = [[frequency, [character, '']]
-            for character, frequency in character_frequency.items()]
-
-    heapq.heapify(heap)
-
-    while len(heap) > 1:
-        left = heapq.heappop(heap)
-        right = heapq.heappop(heap)
-
-        for i in left[1:]:
-            i[1] = '0' + i[1]
-
-        for j in right[1:]:
-            j[1] = '1' + j[1]
-
-        heapq.heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
-
-    tree = sorted(heapq.heappop(heap)[1:], key=lambda x: (len(x[-1]), x))
-
-    '''
-    [['d', '001'], ['e', '010'], ['h', '011'], ['i', '100'], ['r', '110'], 
-    ['T', '0000'], ['b', '0001'], ['o', '1010'], ['s', '1011'], ['t', '1110'], 
-    ['w', '1111']]
-    '''
-    encoded_data = ''.join([i[1] for i in tree])
-
-    return encoded_data, tree
+def sortFreq(freqs):
+    letters = freqs.keys()
+    tuples = []
+    for let in letters:
+        tuples.append((freqs[let], let))
+    tuples.sort()
+    return tuples
 
 
-def huffman_decoding(encoded_data, tree):
+def buildTree(tuples):
+    while len(tuples) > 1:
+        leastTwo = tuple(tuples[0:2])                  # get the 2 to combine
+        theRest = tuples[2:]                          # all the others
+        combFreq = leastTwo[0][0] + leastTwo[1][0]     # the branch points freq
+        # add branch point to the end
+        tuples = theRest + [(combFreq, leastTwo)]
+        # sort it into place
+        tuples = sorted(tuples, key=lambda x: x[0])
+    return tuples[0]  # Return the single tree inside the list
+
+
+def trimTree(tree):
+    # Trim the freq counters off, leaving just the letters
+    p = tree[1]                                    # ignore freq count in [0]
+    if type(p) == type(""):
+        return p              # if just a leaf, return it
+    else:
+        # trim left then right and recombine
+        return (trimTree(p[0]), trimTree(p[1]))
+
+
+def assignCodes(node, pat=''):
+    global codes
+    if type(node) == type(""):
+        codes[node] = pat                # A leaf. set its code
+    else:                              #
+        assignCodes(node[0], pat+"0")    # Branch point. Do the left branch
+        assignCodes(node[1], pat+"1")
+
+
+def encode(s):
+    global codes
     output = ""
-    p = tree
-    for bit in encoded_data:
-        if bit == '0':
-            p = p[1]  # go left
-        else:
-            p = p[0]  # go right
-
-        if type(p) == type(""):
-            output += p  # found a character. Add to output
-            p = tree  # and restart for next character
-
+    for ch in s:
+        output += codes[ch]
     return output
 
 
-############------------ DRIVER CODE ------------############
-if __name__ == "__main__":
+def decode(tree, s):
+    output = ""
+    p = tree
+    for bit in s:
+        if bit == '0':
+            p = p[0]     # Head up the left branch
+        else:
+            p = p[1]     # or up the right branch
+        if type(p) == type(""):
+            output += p              # found a character. Add to output
+            p = tree                 # and restart for next character
+    return output
 
-    input_string = "The bird is the word"
 
-    print("The size of the data is: {}".format(
-        sys.getsizeof(input_string)))
-    print("The content of the data is: {}\n".format(input_string))
+##############------ DRIVER CODE -----------############
+codes = {}
 
-    encoded_data, tree = huffman_encoding(input_string)
+s = 'aaabccdeeeeeffg'
 
-    print("The size of the encoded data is: {}".format(
-        sys.getsizeof(int(encoded_data, base=2))))
-    print("The content of the encoded data is: {}\n".format(encoded_data))
+freqs = frequency(s)
+# print(frequency)
 
-    decoded_data = huffman_decoding(encoded_data, tree)
+tuples = sortFreq(freqs)
+# print(tuples)
 
-    print("The size of the decoded data is: {}".format(
-        sys.getsizeof(decoded_data)))
-    print("The content of the encoded data is: {}\n".format(decoded_data))
+tree = buildTree(tuples)
+trim = trimTree(tree)
+# print(trim)
+
+assignCodes(trim)
+# print(codes)
+
+encoded_data = encode(s)
+print(encoded_data)
+
+print(decode(trim, encoded_data))
