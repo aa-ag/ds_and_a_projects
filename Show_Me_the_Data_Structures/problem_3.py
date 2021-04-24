4973f55cab4034d004f83c7751e402c439fec9c0
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-24 15:27:40 -0500

    found, added and teste version from Chris Meyers and Fred Obermann for problem_3.py: there's a bug, while providing a result, it's not what's expected

--- Show_Me_the_Data_Structures/problem_3.py
+++ Show_Me_the_Data_Structures/problem_3.py
@@ -13,8 +13,8 @@ def huffman_encoding(input_string):
 
     character_frequency = count_character_frequency(input_string)
 
-    heap = [[weight, [symbol, '']]
-            for symbol, weight in character_frequency.items()]
+    heap = [[frequency, [character, '']]
+            for character, frequency in character_frequency.items()]
 
     heapq.heapify(heap)
 
@@ -22,21 +22,40 @@ def huffman_encoding(input_string):
         left = heapq.heappop(heap)
         right = heapq.heappop(heap)
 
-        for value in left[1:]:
-            value[1] = '0' + value[1]
+        for i in left[1:]:
+            i[1] = '0' + i[1]
 
-        for value in right[1:]:
-            value[1] = '1' + value[1]
+        for j in right[1:]:
+            j[1] = '1' + j[1]
 
         heapq.heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
 
-    tree = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+    tree = sorted(heapq.heappop(heap)[1:], key=lambda x: (len(x[-1]), x))
 
-    return ''.join([i[1] for i in tree]), tree
+    '''
+    [['d', '001'], ['e', '010'], ['h', '011'], ['i', '100'], ['r', '110'], 
+    ['T', '0000'], ['b', '0001'], ['o', '1010'], ['s', '1011'], ['t', '1110'], 
+    ['w', '1111']]
+    '''
+    encoded_data = ''.join([i[1] for i in tree])
+
+    return encoded_data, tree
 
 
-# def huffman_decoding(data, tree):
-#     pass
+def huffman_decoding(encoded_data, tree):
+    output = ""
+    p = tree
+    for bit in encoded_data:
+        if bit == '0':
+            p = p[0]  # go left
+        else:
+            p = p[1]  # go right
+
+        if type(p) == type(""):
+            output += p  # found a character. Add to output
+            p = tree  # and restart for next character
+
+    return output
 
 
 ############------------ DRIVER CODE ------------############
@@ -54,8 +73,8 @@ if __name__ == "__main__":
         sys.getsizeof(int(encoded_data, base=2))))
     print("The content of the encoded data is: {}\n".format(encoded_data))
 
-    # decoded_data = huffman_decoding(encoded_data, tree)
+    decoded_data = huffman_decoding(encoded_data, tree)
 
-    # print("The size of the decoded data is: {}\n".format(
-    #     sys.getsizeof(decoded_data)))
-    # print("The content of the encoded data is: {}\n".format(decoded_data))
+    print("The size of the decoded data is: {}\n".format(
+        sys.getsizeof(decoded_data)))
+    print("The content of the encoded data is: {}\n".format(decoded_data))
