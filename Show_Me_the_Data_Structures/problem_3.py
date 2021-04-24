528711e3c3cfd7c7c1fdeae60477d74d7015fab9
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-24 14:02:13 -0500

    reworked problem_3.py: now only using heapq library instead of bot using it and also creating class for MinQueue and Node

--- Show_Me_the_Data_Structures/problem_3.py
+++ Show_Me_the_Data_Structures/problem_3.py
@@ -1,101 +1,51 @@
 ############------------ IMPORTS ------------############
 import sys
-import collections as c
-import heapq as hq
-
-
-############------------ HELPER CODE ------------############
-class Node:
-    def __init__(self, character, frequency, left_child, right_child):
-        self.character = character
-        self.frequency = frequency
-        self.left_child = left_child
-        self.right_child = right_child
-
-
-class Heap:
-    def __init__(self):
-        self.heap = []
-
-    def push(self, item):
-        hq.heappush(self.heap, item)
-
-    def pop(self):
-        return hq.heappop(self.heap)
-
-    def peek(self):
-        return self.heap[0]
-
-    def __getitem__(self, item):
-        return self.heap[item]
-
-    def __len__(self):
-        return len(self.heap)
+import collections
+import heapq
 
 
 ############------------ FUNCTIONS ------------############
-def count_character_frequency(data):
-    return c.Counter(data.replace(' ', ''))
+def count_character_frequency(input_string):
+    return collections.Counter(input_string.replace(' ', ''))
 
 
-def huffman_encoding(data):
-    character_frequency = count_character_frequency(data)
-    '''
-    Counter({'h': 2, 'e': 2, 'i': 2, 'r': 2, 'd': 2, 
-    'T': 1, 'b': 1, 's': 1, 't': 1, 'w': 1, 'o': 1})
-    '''
+def huffman_encoding(input_string):
 
-    heap = Heap()
+    character_frequency = count_character_frequency(input_string)
 
-    for value, frequency in sorted(character_frequency.items(), key=lambda x: x[1]):
-        heap.push([frequency, value])
+    heap = [[weight, [symbol, '']]
+            for symbol, weight in character_frequency.items()]
 
-    answer = ''
+    heapq.heapify(heap)
 
     while len(heap) > 1:
+        left = heapq.heappop(heap)
 
-        left = heap.pop()
-        right = heap.pop()
+        right = heapq.heappop(heap)
 
-        for i in left:
-            answer += str(i)
+        for value in left[1:]:
+            value[1] = '0' + value[1]
 
-        for i in right:
-            answer += str(i)
+        for value in right[1:]:
+            value[1] = '1' + value[1]
 
-    print(answer)
+        heapq.heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
+
+    compressed = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+    return ''.join([i[1] for i in compressed])
 
 
-def huffman_decoding(data, tree):
-    pass
+# def huffman_decoding(data, tree):
+#     pass
 
 
 ############------------ DRIVER CODE ------------############
-# h = Heap()
-
-# h.push([1, "first value here"])
-# h.push([2, "second value here"])
-# h.push([3, "third value here"])
-# h.push([4, "fourth value here"])
-
-# print(h.pop())
-
-# for i in h:
-#     print(i)
-
-'''
-[1, 'first value here']
-[2, 'second value here']
-[3, 'third value here']
-[4, 'fourth value here']
-'''
-
 if __name__ == "__main__":
-    # codes = {}
 
-    a_great_sentence = "The bird is the word"
+    input_string = "The bird is the word"
 
-    huffman_encoding(a_great_sentence)
+    print(huffman_encoding(input_string))
 
 # print("The size of the data is: {}\n".format(
 #     sys.getsizeof(a_great_sentence)))
