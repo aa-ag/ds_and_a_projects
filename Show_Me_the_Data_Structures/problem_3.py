3ca9c70eaaf95f72a92851fe1d35a5d739cd036c
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-24 14:12:35 -0500

    renamed variable  to . ├── Basic_Algos │   ├── analysis.txt │   ├── problem_1.py │   ├── problem_2.py │   ├── problem_3.py │   └── problem_4.py ├── README.md ├── Show_Me_the_Data_Structures │   ├── problem_1.py │   ├── problem_2.py │   ├── problem_3.py │   ├── problem_4.py │   ├── problem_5.py │   └── problem_6.py ├── Unscramble_Computer_Science_Problems │   ├── analysis.txt │   ├── calls.csv │   ├── resubmission_april12.zip │   ├── task_0.py │   ├── task_1.py │   ├── task_2.py │   ├── task_3.py │   ├── task_4.py │   └── texts.csv └── __pycache__     └── task_1.cpython-39.pyc
    4 directories, 22 files to accomdate for expected code from excercise file from class

--- Show_Me_the_Data_Structures/problem_3.py
+++ Show_Me_the_Data_Structures/problem_3.py
@@ -20,7 +20,6 @@ def huffman_encoding(input_string):
 
     while len(heap) > 1:
         left = heapq.heappop(heap)
-
         right = heapq.heappop(heap)
 
         for value in left[1:]:
@@ -31,9 +30,9 @@ def huffman_encoding(input_string):
 
         heapq.heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
 
-    compressed = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+    tree = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
 
-    return ''.join([i[1] for i in compressed])
+    return ''.join([i[1] for i in tree]), tree
 
 
 # def huffman_decoding(data, tree):
@@ -45,20 +44,18 @@ if __name__ == "__main__":
 
     input_string = "The bird is the word"
 
-    print(huffman_encoding(input_string))
+    print("The size of the data is: {}\n".format(
+        sys.getsizeof(input_string)))
+    print("The content of the data is: {}\n".format(input_string))
 
-# print("The size of the data is: {}\n".format(
-#     sys.getsizeof(a_great_sentence)))
-# print("The content of the data is: {}\n".format(a_great_sentence))
+    encoded_data, tree = huffman_encoding(input_string)
 
-# encoded_data, tree = huffman_encoding(a_great_sentence)
+    print("The size of the encoded data is: {}\n".format(
+        sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
 
-# print("The size of the encoded data is: {}\n".format(
-#     sys.getsizeof(int(encoded_data, base=2))))
-# print("The content of the encoded data is: {}\n".format(encoded_data))
+    # decoded_data = huffman_decoding(encoded_data, tree)
 
-# decoded_data = huffman_decoding(encoded_data, tree)
-
-# print("The size of the decoded data is: {}\n".format(
-#     sys.getsizeof(decoded_data)))
-# print("The content of the encoded data is: {}\n".format(decoded_data))
+    # print("The size of the decoded data is: {}\n".format(
+    #     sys.getsizeof(decoded_data)))
+    # print("The content of the encoded data is: {}\n".format(decoded_data))
