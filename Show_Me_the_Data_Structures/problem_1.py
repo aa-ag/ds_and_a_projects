50c76b6fa8ad7ce44db67caa4354a959a46b2f90
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-25 18:42:57 -0500

    added complexity analysis of problem_1.py and made docstrings in problem_1.py uniform

--- Show_Me_the_Data_Structures/problem_1.py
+++ Show_Me_the_Data_Structures/problem_1.py
@@ -6,21 +6,27 @@ import collections
 class LRU_Cache():
 
     def __init__(self, capacity):
-        # Initialize class variables
+        '''
+         Initialize class variables
+        '''
         self.capacity = capacity
         self.cache = collections.OrderedDict()
 
-    def get(self, key):
-        # Retrieve item from provided key.
-        # Return -1 if nonexistent.
+    def get_(self, key):
+        '''
+         Retrieve item from provided key.
+         Return -1 if nonexistent.
+        '''
         if key in self.cache:
             print(self.cache[key])
         else:
             print(-1)
 
-    def set(self, key, value):
-        # Set the value if the key is not present in the cache.
-        # If the cache is at capacity remove the oldest item.
+    def set_(self, key, value):
+        '''
+         Set the value if the key is not present in the cache.
+         If the cache is at capacity remove the oldest item.
+        '''
         if len(self.cache) < self.capacity:
             self.cache[key] = value
         else:
@@ -29,25 +35,28 @@ class LRU_Cache():
             print(-1)
 
     def print_cache(self):
+        '''
+         prints out entire `cache` in terminal
+        '''
         print(self.cache)
 
 
 ############------------ DRIVER CODE ------------############
 our_cache = LRU_Cache(5)
 
-our_cache.set(1, 1)  # adds 1, 1 to ordered dictionary/cache
-our_cache.set(2, 2)  # adds 2, 2 to ordered dictionary/cache
-our_cache.set(3, 3)  # adds 3, 3 to ordered dictionary/cache
-our_cache.set(4, 4)  # adds 4, 4 to ordered dictionary/cache
+our_cache.set_(1, 1)  # adds 1, 1 to ordered dictionary/cache
+our_cache.set_(2, 2)  # adds 2, 2 to ordered dictionary/cache
+our_cache.set_(3, 3)  # adds 3, 3 to ordered dictionary/cache
+our_cache.set_(4, 4)  # adds 4, 4 to ordered dictionary/cache
 
-our_cache.get(1)  # returns 1
-our_cache.get(2)  # returns 2
-our_cache.get(9)  # returns -1 because 9 is not present in the cache
+our_cache.get_(1)  # returns 1
+our_cache.get_(2)  # returns 2
+our_cache.get_(9)  # returns -1 because 9 is not present in the cache
 
-our_cache.set(5, 5)  # adds 5, 5 to ordered dictionary/cache
+our_cache.set_(5, 5)  # adds 5, 5 to ordered dictionary/cache
 our_cache.print_cache()  # see full object
-our_cache.set(6, 6)  # returns -1 because the cache reached it's capacity
+our_cache.set_(6, 6)  # returns -1 because the cache reached it's capacity
 # but it also removes oldest item to add 6, 6
 
 our_cache.print_cache()  # see full object
-our_cache.get(3)  # returns 3
+our_cache.get_(3)  # returns 3
