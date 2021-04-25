b3af3cd7726f5c92755b969b910cd4806627b4c7
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-25 12:14:23 -0500

    added docstrings and a few additional comments to clarify concepts/logic in problem_3.py

--- Show_Me_the_Data_Structures/problem_3.py
+++ Show_Me_the_Data_Structures/problem_3.py
@@ -5,35 +5,53 @@ import collections
 
 ############------------ FUNCTIONS ------------############
 def huffman_encoding(s):
+    '''
+     creates empty string, imports global codes variable,
+     iterates through input string `s` and pupolates output
+     string with either a 0 or a 1 depending on frequency 
+     using global variable `codes` which is itself prepared in 
+     function `encode`
+    '''
     global codes
 
-    output = ''
+    huffman_encoded_output = ''
 
     for character in s:
-        output += codes[character]
+        huffman_encoded_output += codes[character]
 
-    return output
+    return huffman_encoded_output
 
 
-def huffman_decoding(tree, s):
-    output = ''
+def huffman_decoding(tree, encoded_data):
+    '''
+     creates empty string, and using the `tree` created
+     in `build_tree()` function, together with ecoded data
+     generated in `huffman_encoding`, iterates over tree to 
+     find what character corresponds with either a 0 or a 1
+    '''
+    huffman_decoded_output = ''
 
     leaf = tree
 
-    for bit in s:
+    for bit in encoded_data:
         if bit == '0':
             leaf = leaf[0]
         else:
             leaf = leaf[1]
+
         if type(leaf) == str:
-            output += leaf
+            huffman_decoded_output += leaf
             leaf = tree
 
-    return output
+    return huffman_decoded_output
 
 
 def generate_pseudo_heap(s):
-
+    '''
+     counts frequency of each character in input string `s`
+     creates a list of charactes called `characters` and generates
+     a pseudo min heap of tuples
+    '''
     character_frequency = collections.Counter(s)
 
     characters = character_frequency.keys()
@@ -49,6 +67,11 @@ def generate_pseudo_heap(s):
 
 
 def build_tree(pseudo_heap):
+    '''
+     builds a tree merging two lowest values from pseudo
+     min heap created in `generate_pseudo_heap` and creating
+     branches and parent nodes
+    '''
     while len(pseudo_heap) > 1:
         two_lower_values = tuple(pseudo_heap[0:2])
 
@@ -62,12 +85,30 @@ def build_tree(pseudo_heap):
 
     tree = pseudo_heap[0]
 
+    '''
+    this is what `tree` looks like:
+
+    (20, ((8, ((4, ((2, 'h'), (2, 'i'))), (4, ((2, 'r'), (2, ((1, 'T'), \
+    (1, 'b'))))))), (12, ((4, ((2, ((1, 'o'), (1, 's'))), (2, ((1, 't'), \
+    (1, 'w'))))), (8, ((4, ' '), (4, ((2, 'd'), (2, 'e')))))))))
+    '''
+
     trimmed_tree = trim_tree(tree)
+
+    '''
+    this is what `trimmed_tree` looks like:
+
+    ((('h', 'i'), ('r', ('T', 'b'))), ((('o', 's'), ('t', 'w')), \
+        (' ', ('d', 'e'))))
+    '''
 
     return trimmed_tree
 
 
 def trim_tree(tree):
+    '''
+     removes frequencies from pseudo min heap
+    '''
     leaf = tree[1]
 
     if type(leaf) == str:
@@ -77,6 +118,11 @@ def trim_tree(tree):
 
 
 def encode(node, branch=''):
+    '''
+     recursively traverses tree and keeps track of 
+     left or right turns (branch) and replaces character in 
+     global variable codes (dictionary) for either a 0 or a 1
+    '''
     global codes
 
     if type(node) == str:
@@ -87,41 +133,33 @@ def encode(node, branch=''):
 
 
 ##############------ DRIVER CODE -----------############
-codes = {}
+if __name__ == "__main__":
+    print()
 
-# s = 'aaabccdeeeeeffg'
-s = 'The bird is the word'
+    codes = {}
 
-pseudo_heap = generate_pseudo_heap(s)
+    s = "The bird is the word"
 
-tree = build_tree(pseudo_heap)
+    pseudo_heap = generate_pseudo_heap(s)
 
-encode(tree)
+    tree = build_tree(pseudo_heap)
 
-encoded_data = huffman_encoding(s)
-print(encoded_data)
+    encode(tree)
 
-print(huffman_decoding(tree, encoded_data))
+    print()
 
-# if __name__ == "__main__":
-#     codes = {}
+    print("The size of the data is: {}".format(
+        sys.getsizeof(s)))
+    print("The content of the data is: {}\n".format(s))
 
-#     a_great_sentence = "The bird is the word"
+    encoded_data = huffman_encoding(s)
 
-#     print()
+    print("The size of the encoded data is: {}".format(
+        sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
 
-#     print("The size of the data is: {}\n".format(
-#         sys.getsizeof(a_great_sentence)))
-#     print("The content of the data is: {}".format(a_great_sentence))
+    decoded_data = huffman_decoding(tree, encoded_data)
 
-#     encoded_data, tree = huffman_encoding(a_great_sentence)
-
-#     print("The size of the encoded data is: {}\n".format(
-#         sys.getsizeof(int(encoded_data, base=2))))
-#     print("The content of the encoded data is: {}".format(encoded_data))
-
-#     decoded_data = huffman_decoding(encoded_data, tree)
-
-#     print("The size of the decoded data is: {}\n".format(
-#         sys.getsizeof(decoded_data)))
-#     print("The content of the encoded data is: {}".format(decoded_data))
+    print("The size of the decoded data is: {}".format(
+        sys.getsizeof(decoded_data)))
+    print("The content of the encoded data is: {}\n".format(decoded_data))
