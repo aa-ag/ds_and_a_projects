1f701051a78a881fe1d5d6b17f409e4f4193dbbd
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-25 11:45:08 -0500

    added trimmed_tree logic into build_tree function within problem_3.py

--- Show_Me_the_Data_Structures/problem_3.py
+++ Show_Me_the_Data_Structures/problem_3.py
@@ -4,56 +4,6 @@ import collections
 
 
 ############------------ FUNCTIONS ------------############
-def generate_pseudo_heap(s):
-
-    character_frequency = collections.Counter(s)
-
-    characters = character_frequency.keys()
-
-    pseudo_heap = []
-
-    for character in characters:
-        pseudo_heap.append((character_frequency[character], character))
-
-    pseudo_heap.sort()
-
-    return pseudo_heap
-
-
-def build_tree(pseudo_heap):
-    while len(pseudo_heap) > 1:
-        two_lower_values = tuple(pseudo_heap[0:2])
-
-        rest_of_pseudo_heap = pseudo_heap[2:]
-
-        merge = two_lower_values[0][0] + two_lower_values[1][0]
-
-        pseudo_heap = rest_of_pseudo_heap + [(merge, two_lower_values)]
-
-        pseudo_heap = sorted(pseudo_heap, key=lambda x: x[0])
-
-    return pseudo_heap[0]
-
-
-def trim_tree(tree):
-    leaf = tree[1]
-
-    if type(leaf) == str:
-        return leaf
-    else:
-        return (trim_tree(leaf[0]), trim_tree(leaf[1]))
-
-
-def encode(node, pat=''):
-    global codes
-
-    if type(node) == str:
-        codes[node] = pat
-    else:
-        encode(node[0], pat + '0')
-        encode(node[1], pat + '1')
-
-
 def huffman_encoding(s):
     global codes
 
@@ -82,6 +32,60 @@ def huffman_decoding(tree, s):
     return output
 
 
+def generate_pseudo_heap(s):
+
+    character_frequency = collections.Counter(s)
+
+    characters = character_frequency.keys()
+
+    pseudo_heap = []
+
+    for character in characters:
+        pseudo_heap.append((character_frequency[character], character))
+
+    pseudo_heap.sort()
+
+    return pseudo_heap
+
+
+def build_tree(pseudo_heap):
+    while len(pseudo_heap) > 1:
+        two_lower_values = tuple(pseudo_heap[0:2])
+
+        rest_of_pseudo_heap = pseudo_heap[2:]
+
+        merge = two_lower_values[0][0] + two_lower_values[1][0]
+
+        pseudo_heap = rest_of_pseudo_heap + [(merge, two_lower_values)]
+
+        pseudo_heap = sorted(pseudo_heap, key=lambda x: x[0])
+
+    tree = pseudo_heap[0]
+
+    trimmed_tree = trim_tree(tree)
+
+    return trimmed_tree
+
+
+def trim_tree(tree):
+    leaf = tree[1]
+
+    if type(leaf) == str:
+        return leaf
+    else:
+        return trim_tree(leaf[0]), trim_tree(leaf[1])
+
+
+def encode(node, branch=''):
+    global codes
+
+    if type(node) == str:
+        codes[node] = branch
+    else:
+        encode(node[0], branch + '0')
+        encode(node[1], branch + '1')
+
+
 ##############------ DRIVER CODE -----------############
 codes = {}
 
@@ -92,14 +96,12 @@ pseudo_heap = generate_pseudo_heap(s)
 
 tree = build_tree(pseudo_heap)
 
-trim = trim_tree(tree)
-
-encode(trim)
+encode(tree)
 
 encoded_data = huffman_encoding(s)
 print(encoded_data)
 
-print(huffman_decoding(trim, encoded_data))
+print(huffman_decoding(tree, encoded_data))
 
 # if __name__ == "__main__":
 #     codes = {}
