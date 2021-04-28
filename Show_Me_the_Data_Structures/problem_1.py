f4480fe2405fec29fcfc5e3396baf3e0b6a93faf
aa-ag <aaron.aguerrevere@gmail.com>
2021-04-27 21:06:09 -0500

    added tests logic to problem_1.py within showmedatastructures

--- Show_Me_the_Data_Structures/problem_1.py
+++ Show_Me_the_Data_Structures/problem_1.py
@@ -9,11 +9,8 @@ class LRU_Cache:
         '''
          Initialize class variables
         '''
-        if type(capacity) == int and capacity > 0:
-            self.capacity = capacity
-            self.cache = collections.OrderedDict()
-        else:
-            raise "invalid input"
+        self.capacity = capacity
+        self.cache = collections.OrderedDict()
 
     def get_(self, key):
         '''
@@ -30,6 +27,7 @@ class LRU_Cache:
          Set the value if the key is not present in the cache.
          If the cache is at capacity remove the oldest item.
         '''
+
         if len(self.cache) < self.capacity:
             self.cache[key] = value
         else:
@@ -45,8 +43,8 @@ class LRU_Cache:
 
 
 ############------------ DRIVER CODE ------------############
-if __name__ == "__main__":
-    # TEST CASE 1
+# TEST CASE 1
+def test_case_1():
     our_cache = LRU_Cache(5)
 
     our_cache.set_(1, 1)  # adds 1, 1 to ordered dictionary/cache
@@ -66,10 +64,30 @@ if __name__ == "__main__":
     our_cache.print_cache()  # see full object
     our_cache.get_(3)  # returns 3
 
-    # TEST CASE 2
-    our_cache = LRU_Cache(-1)
+
+# TEST CASE 2
+def test_case_2():
+    our_cache = LRU_Cache([])
+
+    our_cache.set_(1, 1)  # adds 1, 1 to ordered dictionary/cache
+    our_cache.set_(2, 2)  # adds 2, 2 to ordered dictionary/cache
+    our_cache.set_(3, 3)  # adds 3, 3 to ordered dictionary/cache
+
     our_cache.print_cache()
 
-    # TEST CASE 3
-    our_cache = LRU_Cache([])
+
+# TEST CASE 3
+def test_case_3():
+    our_cache = LRU_Cache(3)
+
+    our_cache.set_(1, 1)  # adds 1, 1 to ordered dictionary/cache
+    our_cache.set_(2, 2)  # adds 2, 2 to ordered dictionary/cache
+    our_cache.set_(3, 3)  # adds 3, 3 to ordered dictionary/cache
+
     our_cache.print_cache
+
+
+if __name__ == "__main__":
+    test_case_1()
+    # test_case_2()
+    # test_case_3()
