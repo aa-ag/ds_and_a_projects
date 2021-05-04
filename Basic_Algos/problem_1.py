7ec737ec46027f98fe5c713c8532a02def4dac70
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-04 18:15:46 -0500

    added test functions in expected format for problem_1.py

--- Basic_Algos/problem_1.py
+++ Basic_Algos/problem_1.py
@@ -28,28 +28,20 @@ https://www.geeksforgeeks.org/square-root-of-an-integer/
 '''
 
 
-############------------ IMPORTS ------------############
-
-
 ############------------ FUNCTIONS ------------############
 def sqrt(n):
     """
-    Calculate the floored square root of a number
-
-    Args:
-    number(int): Number to find the floored squared root
-    Returns:
-    int: Floored Square Root
+    creates base case for n = 0/1
+    sets a variable i to 1
+    to try everything from 1 up to
+    i * i >= n
     """
-    # base case
+
     if n == 0 or n == 1:
         return n
 
-    # start at 1
     i = 1
     floored_squared_root = 1
-    # and try all numbers until
-    # i * i >= n
 
     while floored_squared_root <= n:
         i += 1
@@ -58,12 +50,54 @@ def sqrt(n):
     return i - 1
 
 ############------------ TESTS ------------############
+# TEST 1
+
+
+def test_case_1():
+    print("Pass" if (3 == sqrt(9)) else "Fail")
+
+
+# TEST 2
+def test_case_2():
+    print("Pass" if (0 == sqrt(0)) else "Fail")
+
+
+# TEST 3
+def test_case_3():
+    print("Pass" if (4 == sqrt(16)) else "Fail")
+
+
+# TEST 4
+def test_case_4():
+    print("Pass" if (1 == sqrt(1)) else "Fail")
+
+
+# TEST 5
+def test_case_5():
+    print("Pass" if (5 == sqrt(27)) else "Fail")
+
+
+# TEST 6
+def test_case_6():
+    pass
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == '__main__':
-    print("Pass" if (3 == sqrt(9)) else "Fail")
-    print("Pass" if (0 == sqrt(0)) else "Fail")
-    print("Pass" if (4 == sqrt(16)) else "Fail")
-    print("Pass" if (1 == sqrt(1)) else "Fail")
-    print("Pass" if (5 == sqrt(27)) else "Fail")
+    # TEST 1
+    test_case_1()
+
+    # TEST 2
+    test_case_2()
+
+    # TEST 3
+    test_case_3()
+
+    # TEST 4
+    test_case_4()
+
+    # TEST 5
+    test_case_5()
+
+    # TEST 6
+    test_case_6()
