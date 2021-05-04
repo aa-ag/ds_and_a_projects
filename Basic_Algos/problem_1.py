b20e3d71c0db3bcb0da0a146ca57760191c70c01
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-04 18:32:00 -0500

    added condifional check for invalid intpus (inputs that are not ints for problem_1.py in ba: only to do left is explanation

--- Basic_Algos/problem_1.py
+++ Basic_Algos/problem_1.py
@@ -49,55 +49,75 @@ def sqrt(n):
 
     return i - 1
 
+
+def input_is_valid(n):
+    '''
+    checks if input `n` is valid (int)
+    '''
+    if type(n) == int:
+        return True
+    return False
+
+
 ############------------ TESTS ------------############
 # TEST 1
-
-
-def test_case_1():
-    print("Pass" if (3 == sqrt(9)) else "Fail")
+def test_case_1(n):
+    if input_is_valid(n) == True:
+        return "Pass" if (n == sqrt(9)) else "Fail"
+    return "Invalid input"
 
 
 # TEST 2
-def test_case_2():
-    print("Pass" if (0 == sqrt(0)) else "Fail")
+def test_case_2(n):
+    if input_is_valid(n) == True:
+        return "Pass" if (n == sqrt(0)) else "Fail"
+    return "Invalid input"
 
 
 # TEST 3
-def test_case_3():
-    print("Pass" if (4 == sqrt(16)) else "Fail")
+def test_case_3(n):
+    if input_is_valid(n) == True:
+        return "Pass" if (n == sqrt(16)) else "Fail"
+    return "Invalid input"
 
 
 # TEST 4
-def test_case_4():
-    print("Pass" if (1 == sqrt(1)) else "Fail")
+def test_case_4(n):
+    if input_is_valid(n) == True:
+        return "Pass" if (n == sqrt(1)) else "Fail"
+    return "Invalid input"
 
 
 # TEST 5
-def test_case_5():
-    print("Pass" if (5 == sqrt(27)) else "Fail")
+def test_case_5(n):
+    if input_is_valid(n) == True:
+        return "Pass" if (n == sqrt(27)) else "Fail"
+    return "Invalid input"
 
 
 # TEST 6
-def test_case_6():
-    pass
+def test_case_6(n):
+    if input_is_valid(n) == True:
+        return "N\\A"
+    return "Invalid input"
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == '__main__':
     # TEST 1
-    test_case_1()
+    print(test_case_1(3))
 
     # TEST 2
-    test_case_2()
+    print(test_case_2(0))
 
     # TEST 3
-    test_case_3()
+    print(test_case_3(4))
 
     # TEST 4
-    test_case_4()
+    print(test_case_4(1))
 
     # TEST 5
-    test_case_5()
+    print(test_case_5(5))
 
     # TEST 6
-    test_case_6()
+    print(test_case_6('this is a string'))
