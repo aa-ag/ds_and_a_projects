8cc91b08f91b696a3640189ea4ffcf8e3c0ca798
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-10 22:04:45 -0500

    using the test function provided in excercise, passing both tests for problem_3.py

--- Basic_Algos/problem_3.py
+++ Basic_Algos/problem_3.py
@@ -32,7 +32,9 @@ def rearrange_digits(input_list):
 
     s = ''.join(str(i) for i in sorted(input_list, reverse=True))
 
-    # return s  # [5, 4, 3, 2, 1]
+    # return s
+    # 54321
+    # 986542
 
     for i in range(len(s)):
         if i % 2 == 0:
@@ -44,23 +46,24 @@ def rearrange_digits(input_list):
 
 
 # odd lenght
-print(rearrange_digits([1, 2, 3, 4, 5]))
+# print(rearrange_digits([1, 2, 3, 4, 5]))
 # [542, 31]
 
 # even length
-print(rearrange_digits([4, 6, 2, 5, 9, 8]))
+# print(rearrange_digits([4, 6, 2, 5, 9, 8]))
 # [964, 852]
 
+
 ############------------ TESTS ------------############
-# def test_function(test_case):
-#     output = rearrange_digits(test_case[0])
-#     solution = test_case[1]
-#     if sum(output) == sum(solution):
-#         print("Pass")
-#     else:
-#         print("Fail")
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
 
 
 ############------------ DRIVER CODE ------------############
-# test_function([[1, 2, 3, 4, 5], [542, 31]])
-# test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function([[1, 2, 3, 4, 5], [542, 31]])
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
