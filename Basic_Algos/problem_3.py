ec29bcf399e1b5f55aab5b760352163d27d08f80
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-12 22:42:34 -0500

    added mergesort logic to problem_3.py to sort array before rearranging it to yield two largest sums

--- Basic_Algos/problem_3.py
+++ Basic_Algos/problem_3.py
@@ -14,56 +14,66 @@ Another expected answer can be [542, 31].
 In scenarios such as these when there are more
 than one possible answers, return any one.
 '''
-############------------ IMPORTS ------------############
 
 
 ############------------ FUNCTIONS ------------############
-def rearrange_digits(input_list):
-    """
-    Rearrange Array Elements so as to form two number such that their sum is maximum.
+def mergesort(items):
+    # case where input is list of 1
+    if len(items) <= 1:
+        return items
 
-    Args:
-       input_list(list): Input List
-    Returns:
-       (int),(int): Two maximum sums
-    """
-    l = ''
-    r = ''
+    middle_index = len(items) // 2
+    left = items[:middle_index]
+    right = items[middle_index:]
 
-    s = ''.join(str(i) for i in sorted(input_list, reverse=True))
+    left = mergesort(left)
+    right = mergesort(right)
 
-    # return s
-    # 54321
-    # 986542
+    return merge(left, right)
 
-    for i in range(len(s)):
-        if i % 2 == 0:
-            l += s[i]
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    # Move through the lists until we have exhausted one
+    while left_index < len(left) and right_index < len(right):
+        # If left's item is larger, append right's item
+        # and increment the index
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        # Otherwise, append left's item and increment
         else:
-            r += s[i]
+            merged.append(left[left_index])
+            left_index += 1
 
-    return [int(l), int(r)]
+    # Append any leftovers. Because we've broken from our while loop,
+    # we know at least one is empty, and the remaining:
+    # a) are already sorted
+    # b) all sort past our last element in merged
+    merged += left[left_index:]
+    merged += right[right_index:]
+
+    # return the ordered, merged list
+    return merged
 
 
-# odd lenght
-# print(rearrange_digits([1, 2, 3, 4, 5]))
-# [542, 31]
-
-# even length
-# print(rearrange_digits([4, 6, 2, 5, 9, 8]))
-# [964, 852]
+print(mergesort([4, 6, 2, 5, 9, 8]))
+# [2, 4, 5, 6, 8, 9]
 
 
 ############------------ TESTS ------------############
-def test_function(test_case):
-    output = rearrange_digits(test_case[0])
-    solution = test_case[1]
-    if sum(output) == sum(solution):
-        print("Pass")
-    else:
-        print("Fail")
+# def test_function(test_case):
+#     output = rearrange_digits(test_case[0])
+#     solution = test_case[1]
+#     if sum(output) == sum(solution):
+#         print("Pass")
+#     else:
+#         print("Fail")
 
 
 ############------------ DRIVER CODE ------------############
-test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
+# test_function([[1, 2, 3, 4, 5], [542, 31]])
+# test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
