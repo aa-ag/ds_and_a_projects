2f3614b0eadf41675033bbdcdad51894019cef56
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-13 20:53:47 -0500

    added binary search for problem_2.py

--- Basic_Algos/problem_2.py
+++ Basic_Algos/problem_2.py
@@ -13,57 +13,71 @@ Example:
 
 Input: nums = [4,5,6,7,0,1,2], target = 0, Output: 4
 '''
-############------------ IMPORTS ------------############
+
+"""
+Find the index by searching in a rotated sorted array
+
+Args:
+    input_list(array), number(int): Input array to search and the target
+Returns:
+    int: Index or -1
+"""
 
 
 ############------------ FUNCTIONS ------------############
+#  def rotated_array_search(input_list, number):
 
-def contains(target, source):
-    if len(source) == 0:
-        return False
-    center = (len(source)-1) // 2
-    if source[center] == target:
-        return True
-    elif source[center] < target:
-        return contains(target, source[center+1:])
-    else:
-        return contains(target, source[:center])
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
 
+    while low <= high:
 
-def rotated_array_search(input_list, number):
-    """
-    Find the index by searching in a rotated sorted array
+        mid = (high + low) // 2
 
-    Args:
-       input_list(array), number(int): Input array to search and the target
-    Returns:
-       int: Index or -1
-    """
-    if number in input_list:
-        return input_list.index(number)
+        # If x is greater, ignore left half
+        if arr[mid] < x:
+            low = mid + 1
+
+        # If x is smaller, ignore right half
+        elif arr[mid] > x:
+            high = mid - 1
+
+        # means x is present at mid
+        else:
+            return mid
+
+    # If we reach here, then the element was not present
     return -1
 
 
-def linear_search(input_list, number):
-    for index, element in enumerate(input_list):
-        if element == number:
-            return index
-    return -1
+print(binary_search([6, 7, 8, 9, 10, 1, 2, 3, 4], 6))
+print(binary_search([6, 7, 8, 9, 10, 1, 2, 3, 4], 1))
+print(binary_search([6, 7, 8, 1, 2, 3, 4], 8))
+print(binary_search([6, 7, 8, 1, 2, 3, 4], 1))
+print(binary_search([6, 7, 8, 1, 2, 3, 4], 10))
+
+# def linear_search(input_list, number):
+#     for index, element in enumerate(input_list):
+#         if element == number:
+#             return index
+#     return -1
 
 
 ############------------ TESTS ------------############
-def test_function(test_case):
-    input_list = test_case[0]
-    number = test_case[1]
-    if linear_search(input_list, number) == rotated_array_search(input_list, number):
-        print("Pass")
-    else:
-        print("Fail")
+# def test_function(test_case):
+#     input_list = test_case[0]
+#     number = test_case[1]
+#     if linear_search(input_list, number) == binary_search(input_list, number):
+#         print("Pass")
+#     else:
+#         print("Fail")
 
 
 ############------------ DRIVER CODE ------------############
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+# test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+# test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+# test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+# test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+# test_function([[6, 7, 8, 1, 2, 3, 4], 10])
