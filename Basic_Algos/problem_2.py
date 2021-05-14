1c5d2b8136987f4b0693c92170a7f28b0ee19f9d
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-13 21:21:41 -0500

    added binary search in sorted rotated array algo, passing all tests

--- Basic_Algos/problem_2.py
+++ Basic_Algos/problem_2.py
@@ -1,60 +1,39 @@
 '''
-You are given a sorted array which is rotated at some random pivot point.
-
-Example: [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]
-
-You are given a target value to search.
-If found in the array return its index, otherwise return -1.
-
-You can assume there are no duplicates in the array
-and your algorithm's runtime complexity must be in the order of O(log n).
-
-Example:
-
-Input: nums = [4,5,6,7,0,1,2], target = 0, Output: 4
+TIME COMPLEXITY: .
+SPACE COMPLEXITY: .
 '''
-
-"""
-Find the index by searching in a rotated sorted array
-
-Args:
-    input_list(array), number(int): Input array to search and the target
-Returns:
-    int: Index or -1
-"""
 
 
 ############------------ FUNCTIONS ------------############
 def rotated_array_search(input_list, number):
     '''
-     performs binary search in input list
-     looking for `number` to return `number`'s index
-     or -1 if `number` isn't present
+
     '''
-    # set pointers:
-    # index all the way left/first index
-    start_index = 0
-    # index all the way right/last index
-    end_index = len(input_list) - 1
+    # set pointers
+    low = 0
+    high = len(input_list)
 
-    middle_index = 0
+    # set conditions to break out of the loop
+    while low < high:
+        # set middle index
+        mid = low + (high-low)//2
+        # base case
+        # if the element in the middle index is the number
+        # return its index
+        if input_list[mid] == number:
+            return mid
 
-    while start_index <= end_index:
-
-        middle_index = (end_index + start_index) // 2
-
-        if input_list[middle_index] == number:
-            return middle_index
-
-        # If number is greater, ignore left half
-        if input_list[middle_index] < number:
-            start_index = middle_index + 1
-
-        # If number is smaller, ignore right half
-        elif input_list[middle_index] > number:
-            end_index = middle_index - 1
-
-    # Alternatively, then the element is not in `input_list`
+        if input_list[low] <= input_list[mid]:
+            if number >= input_list[low] and number < input_list[mid]:
+                high = mid
+            else:
+                low = mid+1
+        else:
+            if number <= input_list[high-1] and number > input_list[mid]:
+                low = mid+1
+            else:
+                high = mid
+    # alternatively, the number isn't in the list
     return -1
 
 
@@ -67,10 +46,10 @@ def linear_search(input_list, number):
 
 ############------------ TESTS ------------############
 def test_function(test_case):
+
     input_list = test_case[0]
     number = test_case[1]
-    print(linear_search(input_list, number))
-    print(rotated_array_search(input_list, number))
+
     if linear_search(input_list, number) == rotated_array_search(input_list, number):
         print("Pass")
     else:
