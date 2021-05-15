45769476544becac9bb28e66d808e2403ba75701
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-14 20:34:25 -0500

    added space complexity for tries in problem_5.py

--- Basic_Algos/problem_5.py
+++ Basic_Algos/problem_5.py
@@ -4,7 +4,7 @@ TIME COMPLEXITY: O(W*L),
                     and L is an average length of the word: 
                     you need to perform L lookups on the average 
                     for each of the W words in the set
-SPACE COMPLEXITY: 
+SPACE COMPLEXITY: O(ALPHABET_SIZE * key_length * N) where N is number of keys in Trie.
 https://stackoverflow.com/questions/13032116/trie-complexity-and-searching
 https://medium.com/basecs/trying-to-understand-tries-3ec6bede0014
 https://www.cs.cmu.edu/~fp/courses/15122-f10/lectures/18-tries.pdf
