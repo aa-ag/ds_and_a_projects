a134bc454dce0aaa1be2632a612b02c1723811b9
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-15 16:55:53 -0500

    added space and time complexity of problem_7.py

--- Basic_Algos/problem_7.py
+++ Basic_Algos/problem_7.py
@@ -1,3 +1,8 @@
+'''
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(ALPHABET_SIZE * key_length * N) where N is number of keys in Trie.
+'''
+
 ############------------ IMPORTS ------------############
 from collections import defaultdict
 
