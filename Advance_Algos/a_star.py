01338f0d7c92671f7030a8c24b061cac8babbeac
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-18 17:21:56 -0500

    added a_star.py into Advanced_Algos folder

---
+++ Advance_Algos/a_star.py
@@ -0,0 +1,23 @@
+############------------ IMPORTS ------------############
+
+############------------ GLOBAL VARIABLES ------------############
+
+############------------ FUNCTIONS ------------############
+def a_star():
+    '''
+     1. generate lists with all possible next steps 
+        towards goal from current position
+
+     2. create a priority queue to store all children
+        based on distance to goal, closest first
+
+     3. set a loop which selects closest child 
+        and repats entire loop until goal 
+        reached or no more children 
+    '''
+    pass
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    pass
