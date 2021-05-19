a997e36e651aca2058c1e0ff8921b79ba02daaa8
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-18 19:37:15 -0500

    added comment with basic formula for a_star.py

--- Advance_Algos/a_star.py
+++ Advance_Algos/a_star.py
@@ -16,3 +16,11 @@ def a_star():
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
     a_star()
+
+'''
+f(n) = g(n) + h(n)
+
+g  (n) : The actual cost path from the start node to the current node. 
+h (n) : The actual cost path from the current node to goal node.
+f  (n) : The actual cost path from the start node to the goal node.
+'''
