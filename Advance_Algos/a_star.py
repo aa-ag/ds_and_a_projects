83c823e3e09d71bfc61b12dce2d3a6aa90bedc84
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-20 19:01:14 -0500

    added add method to PriorityQueue class in a_star.py

--- Advance_Algos/a_star.py
+++ Advance_Algos/a_star.py
@@ -10,6 +10,8 @@ class PriorityQueue:
         for value in iterable:
             heapq.heappush(self.heap, (0, value))
 
+    def add(self, value, priority=0):
+        heapq.heappush(self.heap, (priority, value))
 
 ############------------ FUNCTIONS ------------############
 
